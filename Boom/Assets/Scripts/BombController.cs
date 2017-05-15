0ced0fce7d0fbf5efede30e41cc32a192134f61d
Edo <edo.behmen@gmail.com>
2017-05-15 12:57:10 +0200

    started working with explosions

---
+++ Boom/Assets/Scripts/BombController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BombController : MonoBehaviour {
+	public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("Explode", 3);
+		Destroy (gameObject, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void Explode(){
+		RaycastHit hit;
+		int expLimit = 0;
+		if (Physics.Raycast (transform.position, Vector3.right, out hit)) {
+			expLimit = Math.Min((int)hit.distance / 5, 5);
+		}
+		for (int i= 0; i < expLimit; i++) {
+			Instantiate (explosion, transform.position + 5*i*Vector3.right, Quaternion.identity);
+		}
+		if (Physics.Raycast (transform.position, -Vector3.right, out hit)) {
+			expLimit = Math.Min((int)hit.distance / 5, 5);
+		}
+		for (int i= 0; i < expLimit; i++) {
+			Instantiate (explosion, transform.position -5*i*Vector3.right, Quaternion.identity);
+		}
+		if (Physics.Raycast (transform.position, Vector3.forward, out hit)) {
+			expLimit = Math.Min((int)hit.distance / 5, 5);
+		}
+		for (int i= 0; i < expLimit; i++) {
+			Instantiate (explosion, transform.position + 5*i*Vector3.forward, Quaternion.identity);
+		}
+		if (Physics.Raycast (transform.position, -Vector3.forward, out hit)) {
+			expLimit = Math.Min((int)hit.distance / 5, 5);
+		}
+		for (int i= 0; i < expLimit; i++) {
+			Instantiate (explosion, transform.position -5*i*Vector3.forward, Quaternion.identity);
+		}
+	}
+}
---
+++ Boom/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public int speed = 10;
+	public GameObject bomb;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var moveHorizontal = speed * Time.deltaTime * Input.GetAxis ("Horizontal");
+		var moveVertical = speed * Time.deltaTime * Input.GetAxis ("Vertical");
+		transform.position = transform.position + new Vector3 (moveHorizontal, 0, moveVertical);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Instantiate(bomb, transform.position, Quaternion.identity);
+		}
+	}
+}
